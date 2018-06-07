6e905f4bf076a4a72a44bb174bc1dc7dfd8866e6
Raghu Betina <raghu@firstdraft.com>
2018-06-07 16:13:07 +0000

    Only review user can access show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :current_user_must_be_review_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_review_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_review_user
     review = Review.find(params[:id])
