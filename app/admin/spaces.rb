471ee2be17eea34692a6f1b186dfd569a784b1fc
Raghu Betina <raghu@firstdraft.com>
2018-06-07 16:12:33 +0000

    Whitelist columns in Space for ActiveAdmin

--- app/admin/spaces.rb
+++ app/admin/spaces.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Space do
+
+ permit_params :address, :phone_number, :picture
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
